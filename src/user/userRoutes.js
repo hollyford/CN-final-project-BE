746950c3a512821b12f8c7ae2bdd1b61ecbc1460
Holly Ford <holly_ford@hotmail.com>
2022-01-05 12:06:08 +0000

    updated userRouters

--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -3,10 +3,10 @@ const { addUser, deleteUser } = require("./userController");
 const { hashPassword, checkPassword, checkEmail } = require("../middleware");
 const userRouter = Router();
 
-userRouter.post("/user", addUser);
+userRouter.post("/user", checkEmail, hashPassword, addUser);
 userRouter.delete("/deleteUser", deleteUser);
 userRouter.get("/user", listUsers);
 userRouter.put("/user", updateUsers);
-userRouter.post("/login", passwordMatch)
+userRouter.post("/login", checkPassword)
 
 module.exports = userRouter;
