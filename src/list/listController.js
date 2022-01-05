6ef4b551896bd1c4de1fa6977fd1c64ead625b45
Ju1ija <j.lipnickaja@gmail.com>
2022-01-05 10:34:12 +0000

    added option to filter lists

--- src/list/listController.js
+++ src/list/listController.js
@@ -14,8 +14,21 @@ exports.addList = async (req, res) => {
 exports.readLists = async (req, res) => {
   try {
     const allLists = await List.find({});
-    const message = allLists.length === 0 ? "No lists found." : `${allLists.length} lists found.`;
+    const message = allLists.length === 0 ? "No lists found" : `${allLists.length} lists found`;
     res.status(200).send({ message: message, allLists });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Unsuccessful, please try again later" });
+  }
+}
+
+exports.filterLists = async (req, res) => {
+  try {
+    const listObj = {};
+    listObj[req.query.filteredBy] = req.query.value;
+    const filteredLists = await List.find(listObj);
+    const message = filteredLists.length === 0 ? "No lists found" : `${filteredLists.length} list(s) found`;
+    res.status(200).send({ message: message, filteredLists });
   } catch (error) {
     console.log(error);
     res.status(500).send({ message: "Unsuccessful, please try again later" });
