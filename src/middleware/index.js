d54bdfabbbf02a160665c0c8e777e2bc69043ed8
Rachel Groves <grovrac09@gmail.com>
2022-01-05 11:12:14 +0000

    updated middleware

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,40 +1,39 @@
 const bcrypt = require("bcrypt");
-// const User = require("../user/userModel");
+const User = require("../user/userModel");
 
 exports.hashPassword = async (req, res, next) => {
     try {
         req.body.password = await bcrypt.hash(req.body.password, 8);
         next();
     } catch (error) {
-        res.status(500).send({ message: "error 2" });
+        res.status(500).send({ message: "Unsuccessful, please try again." });
     }
 };
 
 exports.checkPassword = async (req, res, next) => {
     try {
         // const findPw = await User.findById({ _id: req.body._id });
-        const findPw = await User.findByOne({ username: req.body.username })
-        const checker = await bcrypt.compare(req.body.password, findPw.password);
+        const findUser = await User.findOne({ username: req.body.username })
+        const checker = await bcrypt.compare(req.body.password, findUser.password);
         if (checker === true) {
             res.status(200).send({ message: "Successfully checked password." });
-            next();
+            // next();
         } else {
-            res.status(500).send({ message: "Unsuccessful, please try again." });
+            res.status(401).send({ message: "Unsuccessful, please try again." });
         }
     } catch (error) {
-        res.status(500).send({ message: "error 3" });
+        res.status(500).send({ message: "Unsuccessful, please try again." });
     }
 };
 
 exports.checkEmail = async (req, res, next) => {
     try {
-        console.log("test");
         if (/.+\@.+\..+/.test(req.body.email)) {
             next();
         } else {
-            res.status(500).send({ message: "Invalid email address entered." });
+            res.status(400).send({ message: "Invalid email address entered." });
         }
     } catch (error) {
-        res.status(500).send({ message: "error 1" });
+        res.status(500).send({ message: "Unsuccessful, please try again." });
     }
 };
