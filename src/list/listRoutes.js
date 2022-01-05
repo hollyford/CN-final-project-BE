5ef334d21c97e6596ac589b7bbedc2fbd0dbf5b3
Ju1ija <j.lipnickaja@gmail.com>
2022-01-05 12:09:49 +0000

    updated listRoutes

--- src/list/listRoutes.js
+++ src/list/listRoutes.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
-const { addList, readLists } = require("./listController");
+const { addList, readLists, filterLists, findSpecificList } = require("./listController");
 const listRouter = Router();
 
 listRouter.post("/lists", addList);
 listRouter.get("/lists", readLists);
+listRouter.get("/filteredLists", filterLists);
+listRouter.get("/lists/:id", findSpecificList);
 
 module.exports = listRouter;
