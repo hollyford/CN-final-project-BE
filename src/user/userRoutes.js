bae371c565109587b947ca063783e6c3d3d653cd
Ju1ija <85833623+Ju1ija@users.noreply.github.com>
2022-01-05 16:40:04 +0000

    Update userRoutes.js

--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -1,19 +1,15 @@
 const { Router } = require("express");
-const { addUser, deleteUser, findUser, listUsers, updateUser, updateEmail, updatePassword } = require("./userController");
+const { addUser, findUser, listUsers, updateUser, updateEmail, updatePassword, deleteUser } = require("./userController");
 const { hashPassword, checkPassword, checkEmail } = require("../middleware");
 const userRouter = Router();
 
 userRouter.post("/user", checkEmail, hashPassword, addUser);
-userRouter.delete("/user", deleteUser);
+userRouter.post("/login", checkPassword);
 userRouter.get("/user", findUser);
-userRouter.get("/listUser", listUsers);
+userRouter.get("/users", listUsers);
 userRouter.put("/user", updateUser);
 userRouter.put("/userEmail", checkEmail, updateEmail);
 userRouter.put("/userPassword", hashPassword, updatePassword);
-
-// userRouter.get("/profile", userProfile);
-userRouter.post("/login", checkPassword);
-
-
+userRouter.delete("/user", deleteUser);
 
 module.exports = userRouter;
