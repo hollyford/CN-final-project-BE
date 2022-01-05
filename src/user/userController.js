b1f4f95efc2524c8fe712c3034d9da5aa3fa9d46
Holly Ford <holly_ford@hotmail.com>
2022-01-05 16:16:18 +0000

    completed crud functions for user

--- src/user/userController.js
+++ src/user/userController.js
@@ -7,7 +7,7 @@ exports.addUser = async (req, res) => {
       res.status(200).send({ message: `Successfully added ${newUser.username}`, newUser });
     } catch (error) {
       console.log(error);
-      res.status(500).send({ message: "Unsuccessful, please try again later" });
+      res.status(500).send({ message: "Unsuccessful, please try again later (Add user)" });
     }
   }
 
@@ -18,7 +18,58 @@ exports.addUser = async (req, res) => {
           res.status(200).send({ message: `Successfully deleted ${searchUser.username}`});
       } catch (error) {
         console.log(error);
-        res.status(500).send({ message: "Unsuccessful, please try again later" });
+        res.status(500).send({ message: "Unsuccessful, please try again later (Delete user)" });
       }
   }
 
+  exports.listUsers = async (req, res) => {
+    try {
+      const user = await User.find({});
+      res.status(200).send({ user });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({ message: "Unsuccessful, please try again later (List users)" });
+    }
+  } 
+
+  exports.findUser = async (req, res) => {
+    try {
+      const user = await User.findById(req.body._id);
+      res.status(200).send({ user });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({ message: "Unsuccessful, please try again later (Find user)" });
+    }
+  } 
+
+  exports.updateUser = async (req, res) => {
+    try {
+      const user = await User.findByIdAndUpdate({_id: req.body._id}, (req.body),             
+      {upsert: true, new: true, runValidators: true});
+      res.status(200).send({ user });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({ message: "Unsuccessful, please try again later (Update user)" });
+    }
+  } 
+  exports.updateEmail = async (req, res) => {
+    try {
+      const user = await User.findByIdAndUpdate({_id: req.body._id}, (req.body),             
+      {upsert: true, new: true, runValidators: true});
+      res.status(200).send({ user });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({ message: "Unsuccessful, please try again later (Update user)" });
+    }
+  } 
+
+  exports.updatePassword = async (req, res) => {
+    try {
+      const user = await User.findByIdAndUpdate({_id: req.body._id}, (req.body),             
+      {upsert: true, new: true, runValidators: true});
+      res.status(200).send({ user });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({ message: "Unsuccessful, please try again later (Update user)" });
+    }
+  } 
--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -1,12 +1,19 @@
 const { Router } = require("express");
-const { addUser, deleteUser } = require("./userController");
+const { addUser, deleteUser, findUser, listUsers, updateUser, updateEmail, updatePassword } = require("./userController");
 const { hashPassword, checkPassword, checkEmail } = require("../middleware");
 const userRouter = Router();
 
 userRouter.post("/user", checkEmail, hashPassword, addUser);
 userRouter.delete("/user", deleteUser);
-// userRouter.get("/user", listUsers);
-// userRouter.put("/user", updateUsers);
-userRouter.post("/login", checkPassword)
+userRouter.get("/user", findUser);
+userRouter.get("/listUser", listUsers);
+userRouter.put("/user", updateUser);
+userRouter.put("/userEmail", checkEmail, updateEmail);
+userRouter.put("/userPassword", hashPassword, updatePassword);
+
+// userRouter.get("/profile", userProfile);
+userRouter.post("/login", checkPassword);
+
+
 
 module.exports = userRouter;
