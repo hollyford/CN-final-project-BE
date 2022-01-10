3096a019082603e9d11338607aa25e32ca4c0cb5
Ju1ija <j.lipnickaja@gmail.com>
2022-01-10 09:26:12 +0000

    updating user lists controllers

--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { addUser, findUser, listUsers, updateUser, updateEmail, updatePassword, addToLists, deleteUser } = require("./userController");
+const { addUser, findUser, listUsers, updateUser, updateEmail, updatePassword, addToLists, updateListItemCompletionState, deleteUser } = require("./userController");
 const { hashPassword, checkPassword, checkEmail } = require("../middleware");
 const userRouter = Router();
 
@@ -8,6 +8,7 @@ userRouter.post("/login", checkPassword);
 userRouter.get("/users/:id", findUser);
 userRouter.get("/users", listUsers);
 userRouter.patch("/users/:id/lists", addToLists);
+userRouter.patch("/users/:userId/lists/:listId", updateListItemCompletionState);
 userRouter.put("/users/:id", updateUser);
 userRouter.put("/userEmail/:id", checkEmail, updateEmail);
 userRouter.put("/userPassword/:id", hashPassword, updatePassword);
